f692ab4a11ad53a92bce2ebfa0fb18d6985b7658
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-23 17:09:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,14 +17,8 @@ class Transfer
   end 
   
   def execute_transaction 
-    if @sender.balance > @amount && @status = "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount 
-      @status = "complete"
-    else 
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    end 
+    if @sender.balance < @amount 
+      
   end 
   
   def reverse_transfer 
