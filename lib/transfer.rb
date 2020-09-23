6a6d938e8f4a617beae79b69a3b8d1c6baa156dc
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-23 17:04:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,7 @@ class Transfer
   
   def execute_transaction 
     if @sender.balance > @amount && @status = "pending"
-      @sender.balance -= 
+      @sender.balance -= @amount
+      
   end 
 end
