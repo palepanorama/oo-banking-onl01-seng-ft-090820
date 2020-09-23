07750313a096e5f3d340a73bfed56d74e0103329
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-23 16:59:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,6 +2,8 @@ class Transfer
   attr_accessor :sender, :receiver, :status, :amount 
   
   def initialize(sender, receiver, amount)
-    @transfer = transfer 
+    @sender = sender 
+    @receiver = receiver 
+    @amount = amount 
   end 
 end
